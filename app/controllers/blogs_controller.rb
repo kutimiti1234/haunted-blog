9681541d7c2f562e58b345b21f6f3a3c2fd2447b
kutimiti1234 <tarbit.offensive@gmail.com>
2025-01-21 00:45:59 +0900

    refactor: delete unnecessary if-clause

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,11 +10,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = if current_user
-              Blog.accessible(current_user).find(params[:id])
-            else
-              Blog.published.find(params[:id])
-            end
+    @blog = Blog.accessible(current_user).find(params[:id])
   end
 
   def new
